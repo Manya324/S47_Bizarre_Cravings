d0fb93d2406780d7a91fd6fe340e8557c6d64170
Manya324 <manya.j@kalvium.community>
2024-02-04 22:30:57 +0530

    Connected Database to front-end

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const cors = require("cors");
+const {MongoClient} = require("mongodb")
+
+
+app.use(cors());
 
 const uri = "mongodb+srv://Manya_Jain:Gunnu324@cluster1.fvkwdo1.mongodb.net/?retryWrites=true&w=majority";
 
-app.get('/', (req, res) => {
-  mongoose.connect(uri)
-    .then(() => {
-      res.json({ connection: 'Connected' });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.json({ connection: 'Not Connected' });
-    });
-});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    const database = client.db('listData');
+    const collection = database.collection('Weird Items eaten list');
 
-app.get("/ping",(req,res) => {
-  res.send("Hello world!")
-})
+    app.get('/', async (req,res) => {
+      const result = await collection.find({}).toArray();
+      res.json(result);
+    })
+  })
+  .catch(err => {
+    console.log('Error connecting to MongoDB Atlas:', err);
+  });
+
+
 
 app.listen(5000, ()=> {
   console.log(`Server is running on 5000`);
